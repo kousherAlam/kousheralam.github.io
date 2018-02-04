5a0d46fee8e0f3863a4430a8af2b283929dfbb59
kousher alam <kousheralampranto@gmail.com>
2018-02-04 22:16:37 +0600

    add copy code btn

--- js/app.js
+++ js/app.js
@@ -30,4 +30,50 @@ function hasClass(el, className){
     close_btn.addEventListener("click",function(){
         removeClass(main_menu, 'open');
     })
-}())
+}());
+
+
+function selectText(element) {
+    var doc = document
+        , text = element
+        , range, selection
+    ;
+    if (doc.body.createTextRange) { //ms
+        range = doc.body.createTextRange();
+        range.moveToElementText(text);
+        range.select();
+    } else if (window.getSelection) { //all others
+        selection = window.getSelection();
+        range = doc.createRange();
+        range.selectNodeContents(text);
+        selection.removeAllRanges();
+        selection.addRange(range);
+    }
+}
+
+function resetCopyText(elm){
+    elm.forEach(function(elem,i){
+        var btn = elem.querySelector("button");
+        removeClass(btn, 'save-success');
+        btn.innerHTML = "Copy";
+    })
+}
+
+(function(){
+    var highlight = document.querySelectorAll(".highlight");
+    highlight.forEach(function(elem, i){
+        var btn = document.createElement("button");
+            addClass(btn, 'save');
+            btn.textContent = "Copy";
+            elem.appendChild(btn);
+            btn.addEventListener("click",function(){
+                var code_container = elem.querySelector("code");
+                selectText(code_container);
+                document.execCommand('copy');
+                resetCopyText(highlight);
+                btn.innerHTML = 'Copied';
+                addClass(btn, 'save-success');
+            });
+        console.log(elem);
+    })
+}());
