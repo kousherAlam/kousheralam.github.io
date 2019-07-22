c1ac22e9db2637e7253ddd477a0ac4aa35f014f4
kousher alam <kousheralampranto@gmail.com>
2019-07-22 23:19:32 +0600

    updated site base style

---
+++ README.md
@@ -0,0 +1,9 @@
+### https://kousheralam.github.io
+This is my personal website 
+
+TODO
+- Base Style [done]
+- Content
+- Responsive
+- Contact Form 
+- Analitics 
--- js/app.js
+++ js/app.js
@@ -1,6 +1,8 @@
 (function(){
-    // offline first 
-    // updateing catche 
-    // Background Operation 
-    // Firebase contact Form 
+    function _(id){return document.getElementById(id);}
+    function addClass(element){}
+    function removeClass(element){}
+    function show(element){}
+    function hide(element){}
+    function sendMessage(element){}
 }());
