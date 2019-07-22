6d35d97b8a21d1bc5ec8d30ad2d38e61335eb288
kousher alam <kousheralampranto@gmail.com>
2019-07-23 01:42:25 +0600

    added firebase

--- js/app.js
+++ js/app.js
@@ -1,4 +1,16 @@
 (function(){
+    var firebaseConfig = {
+        apiKey: "AIzaSyDJPI-v3nsTE5eXSTEE1nhcFlCesj_A5UQ",
+        authDomain: "mypersonalsite-451454.firebaseapp.com",
+        databaseURL: "https://mypersonalsite-451454.firebaseio.com",
+        projectId: "mypersonalsite-451454",
+        storageBucket: "mypersonalsite-451454.appspot.com",
+        messagingSenderId: "775431614205",
+        appId: "1:775431614205:web:62900e7e8dcf152f"
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+
     function _(id){return document.getElementById(id);}
     function addClass(element){
         
@@ -11,17 +23,54 @@
 
     (function(){
         var contactForm = _("contact-form");
-        var message = _("sending-message");
+        var messageContainer = _("sending-message");
         var overflow = "overflow-hidden";
         var open = "open";
-        contactForm.addEventListener("submit", function(){
+        var errorClass = "is-invalid";
+
+        var email = _("email"), 
+            name = _("name"),
+            message = _("message"),
+            sendReply = _("reply"); 
+        
+        var db = firebase.firestore();
+        contactForm.addEventListener("submit", function(event){
+            event.preventDefault();
+            if(email.value.trim().lenght < 5){
+                email.classList.add(errorClass);
+                return false;
+            }
+            if(name.value.trim().lenght < 4){
+                name.classList.add(errorClass);
+                return false;
+            }
+            if(message.value.trim().lenght < 10){
+                message.classList.add(errorClass);
+                return false;
+            }
+
             document.body.classList.add(overflow)
-            message.classList.add(open);
-    
-            setTimeout(function(){
-                message.classList.remove(open); 
-                document.body.classList.remove(overflow)
-            }, 3000);
+            messageContainer.classList.add(open);
+            db.collection("messages").add({
+                email: email.value,
+                message: message.value,
+                name: name.value, 
+                browser: window.navigator.userAgent,
+                reply: !sendReply.getAttribute('checked'),
+                created_at: new Date(),
+            })
+            .then(function(docRef) {
+                alert("Successfully Message Sended!");
+            })
+            .catch(function(error) {
+                alert("Error Happend");
+            }).then(function(){
+                messageContainer.classList.remove(open); 
+                document.body.classList.remove(overflow);
+                email.value = "";
+                name.value = "";
+                message.value = "";
+            });
         });
     }());
 }());
