f7099ecb200c1ac23fa4f6720c3e66077c38aca6
kousher alam <kousheralampranto@gmail.com>
2020-11-14 16:16:08 +0600

    added loader

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,16 @@
 
 function toggleMenu(){
     var menu = document.getElementById("mobile-menu");
-    console.log('toggle menu is clickeing', menu);
     if(menu.classList.contains('open')){
         menu.classList.remove('open');
     } else {
         menu.classList.add('open');
     }
 }
+
+window.addEventListener('DOMContentLoaded', (event) => {
+    document.body.classList.remove('overflow-hidden');
+    const loader = document.getElementById('loader-holder');
+    if(!loader){ return; }
+    loader.parentElement.removeChild(loader);
+});
