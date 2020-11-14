85fb653dd7eacc254754bcb1f3a316a7692f599d
kousher alam <kousheralampranto@gmail.com>
2020-11-14 20:43:24 +0600

    updated script

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,14 +1,15 @@
-
-function toggleMenu(){
-    var menu = document.getElementById("mobile-menu");
-    if(menu.classList.contains('open')){
-        menu.classList.remove('open');
-    } else {
-        menu.classList.add('open');
+window.addEventListener('DOMContentLoaded', ()=>{
+    function toggleMenu(){
+        var menu = document.getElementById("mobile-menu");
+        if(menu.classList.contains('open')){
+            menu.classList.remove('open');
+        } else {
+            menu.classList.add('open');
+        }
     }
-}
+});
 
-window.addEventListener('DOMContentLoaded', (event) => {
+window.addEventListener('load', () => {
     document.body.classList.remove('overflow-hidden');
     const loader = document.getElementById('loader-holder');
     if(!loader){ return; }
