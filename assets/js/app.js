1afc9bacfb3beaec97fcfb180cd6f5edf2bbb89d
kousher alam <kousheralampranto@gmail.com>
2020-11-16 14:49:57 +0600

    updated website slider

--- assets/js/app.js
+++ assets/js/app.js
@@ -9,7 +9,23 @@ function toggleMenu(){
 
 window.addEventListener('DOMContentLoaded', () => {
     document.body.classList.remove('overflow-hidden');
-    const loader = document.getElementById('loader-holder');
+    var loader = document.getElementById('loader-holder');
     if(!loader){ return; }
     loader.parentElement.removeChild(loader);
+});
+
+window.addEventListener("load", function(){
+    var imageViewContainer = document.getElementsByClassName("image-viewer-slider");
+    if(imageViewContainer && imageViewContainer.length > 0){
+        for(var i=0; i<imageViewContainer.length; i++){
+            var loadeing = imageViewContainer[i].querySelector('div.loading');
+            var slider = imageViewContainer[i].querySelector('ul.d-none');
+            if(loadeing){
+                loadeing.parentElement.removeChild(loadeing);
+            }
+            if(slider){
+                slider.classList.remove('d-none');
+            }
+        }
+    }
 });
