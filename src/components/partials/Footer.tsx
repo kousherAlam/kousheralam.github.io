6d85f3c1adbe46777e1bca45e936b81bc0897e63
Kousher alam <kousheralampranto@gmail.com>
2023-06-23 21:46:06 +0600

    Added hash to see in website

--- src/components/partials/Footer.tsx
+++ src/components/partials/Footer.tsx
@@ -5,18 +5,31 @@ import { AppConfig } from "@/utils/AppConfig";
 const Footer = () => (
   <Section>
     <div className="border-t border-gray-600 pt-5">
-      <div className="text-sm text-gray-200">
-        © Copyright {new Date().getFullYear()} by {AppConfig.site_name}. Built
-        with ♥ by{" "}
-        <a
-          className="text-cyan-400 hover:underline"
-          href="https://creativedesignsguru.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          CreativeDesignsGuru
-        </a>
-        .
+      <div className="grid grid-cols-2">
+        <div>
+          <div className="text-sm text-gray-200">
+            © Copyright {new Date().getFullYear()} by {AppConfig.site_name}.
+            Built with ♥ by{" "}
+            <a
+              className="text-cyan-400 hover:underline"
+              href="https://creativedesignsguru.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              CreativeDesignsGuru
+            </a>
+            .
+          </div>
+        </div>
+        <div>
+          <div className="text-right text-sm">
+            <span className="text-white font-bold">Commit:</span>
+            &nbsp;
+            <span className="text-cyan-400 hover:underline uppercase">
+              {import.meta.env.COMMIT_HASH}
+            </span>
+          </div>
+        </div>
       </div>
     </div>
   </Section>
