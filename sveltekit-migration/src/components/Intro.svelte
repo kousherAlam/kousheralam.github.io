b8a5621c56d8229b10df44c7d1b6c70bafbff885
Kousher alam <kousheralampranto@gmail.com>
2023-09-28 20:27:15 +0600

    added icons, colors and spacing

--- sveltekit-migration/src/components/Intro.svelte
+++ sveltekit-migration/src/components/Intro.svelte
@@ -1,33 +1,43 @@
 <script>
+	import { YoutubeSolid, GithubSolid, LinkedinSolid } from 'flowbite-svelte-icons';
 </script>
 
-<div class="grid grid-cols-6">
-	<div class="col-span-2">
-		<p>Picture</p>
-	</div>
-	<div class="col-span-4">
-		<h1 class="text-4xl">Hi, I'm <span class="font-bold text-orange-400">Kousher Alam</span></h1>
-		<h2 class="text-2xl">Senior Solutions developer @ <span>Otto International</span></h2>
+<div class="grid grid-cols-1 text-center sm:text-left sm:grid-cols-2 md:grid-cols-6">
+	<div class="md:col-span-4 order-2 sm:order-1">
+		<p>Hello there 👋</p>
+		<h1 class="text-4xl">
+			I'm <span class="font-bold text-orange-400">Kousher Alam</span>
+		</h1>
+		<h2 class="text-2xl mt-1">Senior Solutions developer @ <span>Otto International</span></h2>
 		<div>
-			<ul>
+			<ul class="list-none flex space-x-3 mt-1">
 				<li>
-					<a href="https://facebook.com" target="_blank">Facebook</a>
+					<a href="https://facebook.com" target="_blank">
+						<GithubSolid class="h-6 w-6 text-gray-800 mr-4" />
+					</a>
 				</li>
 				<li>
-					<a href="https://facebook.com" target="_blank">Facebook</a>
+					<a href="https://facebook.com" target="_blank">
+						<LinkedinSolid class="h-6 w-6 text-blue-500 mr-4" />
+					</a>
 				</li>
 				<li>
-					<a href="https://facebook.com" target="_blank">Facebook</a>
+					<a href="https://facebook.com" target="_blank">
+						<YoutubeSolid class="h-6 w-6 text-red-500 mr-4" />
+					</a>
 				</li>
 			</ul>
 		</div>
-		<p>
+		<p class="mt-6">
 			My description: Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eaque illum magnam
 			alias dicta recusandae non fuga iusto saepe ullam magni?
 		</p>
 		<div>
 			<p>Skills</p>
 		</div>
+	</div>
+	<div class="order-1 sm:order-2 md:col-span-2 flex justify-center sm:justify-end">
+		<img class="h-80 w-64 sm:pr-4" src="/avatar.svg" alt="Kousher's Profile Picture" />
 	</div>
 </div>
 
--- sveltekit-migration/src/routes/+layout.svelte
+++ sveltekit-migration/src/routes/+layout.svelte
@@ -6,7 +6,7 @@
 	<title>Welcome to Kousher's Webstie</title>
 </svelte:head>
 
-<div class="container mx-auto pt-10 pb-10">
+<div class="container mx-auto pt-10 pb-10 max-w-4xl px-5">
 	<slot />
 </div>
 <div class="py-4 text-center">
