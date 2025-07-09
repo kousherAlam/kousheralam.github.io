6fb91857da5bcd31722a21c82925a70254ff0440
Kousher alam <kousheralampranto@gmail.com>
2025-07-09 20:55:15 +0600

    updated

--- pki/main.go
+++ pki/main.go
@@ -1,7 +1,31 @@
 package main
 
-func export_wasm_func() {
+import "syscall/js"
 
+type CertAltType string
+
+const (
+	CertDNSAltType CertAltType = CertAltType("DNS")
+	CertIPAltType  CertAltType = CertAltType("IP")
+)
+
+type CertAltName struct {
+	Type string
+}
+
+type OneTierCertInfo struct {
+	Org     string
+	Name    string
+	Period  int
+	AltName []CertAltName
+}
+
+func Generate_one_tier_cetificate(data OneTierCertInfo) {
+
+}
+
+func export_wasm_func() {
+	js.Global().Set("generate", js.FuncOf(Generate_one_tier_cetificate(OneTierCertInfo{})))
 }
 
 func main() {
