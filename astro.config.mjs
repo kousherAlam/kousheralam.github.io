27cb4a222fe9e89b34cf23325fe2cb682b014ce0
Kousher alam <kousheralampranto@gmail.com>
2025-07-09 23:07:23 +0600

    upgrade tailwind to v4

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,29 +1,31 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
-import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 import partytown from "@astrojs/partytown";
 import sitemap from '@astrojs/sitemap';
 
 import prefetch from "@astrojs/prefetch";
 
+import tailwindcss from "@tailwindcss/vite";
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://kousheralam.github.io',
+
   server: {
     port: 3000,
     open: false,
   },
+
   integrations: [
-    tailwind({
-      // Example: Disable injecting a basic `base.css` import on every page.
-      // Useful if you need to define and/or import your own custom `base.css`.
-      applyBaseStyles: false
-    }),
     mdx(),
     partytown(),
     sitemap(),
     prefetch(),
     react(),
-  ]
+  ],
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
 });
